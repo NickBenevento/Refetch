585ffc5cd6623e918d54dee77760077302abb4ca
Nick Benevento <nbenevento99@gmail.com>
2025-04-20 15:23:11 -0400

    Add class structure for subscription/notification service

---
+++ api/src/subscriptions/Product.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from api.src.subscriptions.product_status import ProductStatus
+
+class Product:
+
+    def __init__(self, name: str, url: str) -> None:
+        self._product_name = name
+        self._url = url
+        self._last_fetch_time: datetime | None = None
+        # enum?
+        self._status = ProductStatus.PENDING
+
+    
+    def update_url(self, url: str) -> None:
+        """Updates the URL for the product"""
+        # todo: type checking, valid url checking
+        self._url = url
+    
+    def get_status(self) -> ProductStatus:
+        return self._status
+
+    def check_status(self) -> bool:
+        """Scrapes the product URL to check if the product is in stock"""
+        # if item in stock:
+        #   self._status = ProductStatus.IN_STOCK
+        return False
+    
+    def __repr__(self):
+        return f"Product {self._product_name!r}: status={self._status} (url={self._url})"
