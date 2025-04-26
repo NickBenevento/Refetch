c2bac7db220fa557b254f8be1fcc6726650d7823
Nick Benevento <nbenevento99@gmail.com>
2025-04-25 23:15:13 -0400

    Basic CRUD operations, use AnyUrl and remove validators library

--- api/models/pydantic/product.py
+++ api/models/pydantic/product.py
@@ -1,24 +1,20 @@
 import uuid
 from typing import Annotated
 
-import validators
-from pydantic import AfterValidator, BaseModel, Field, ValidationError
-
-
-def check_valid_url(url: str) -> str:
-    if not validators.url(url):
-        raise ValidationError("Invalid url: %s", url)
-    return url
+from pydantic import AnyUrl, BaseModel, Field
 
 
 class Product(BaseModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4)
 
-    url: Annotated[
-        str, AfterValidator(check_valid_url), "The url to the desired product page"
-    ]
+    url: Annotated[AnyUrl, "The url to the desired product page"]
 
     name: Annotated[str, "The name of the product"]
+
+
+class UpdateProduct(BaseModel):
+    url: Annotated[AnyUrl | None, "The new url"]
+    name: Annotated[str | None, "The new name of the product"]
 
 
 class ProductList(BaseModel):
--- api/routers/product.py
+++ api/routers/product.py
@@ -1,8 +1,42 @@
+from uuid import UUID
 from fastapi import APIRouter, status
+
+from ..models.pydantic.product import Product, UpdateProduct
 
 router = APIRouter(prefix="/products", tags=["products"])
 
 
+# Create
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_products(product: Product):
+    return product
+
+
+# Read
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_products():
     return {"products": "all products"}
+
+
+@router.get("/{item_id}", status_code=status.HTTP_200_OK)
+async def get_products(item_id: UUID):
+    # get product by id from db
+    # if not item_id 
+    # 
+    return {"products": "all products"}
+
+
+# Update
+@router.put("/", status_code=status.HTTP_200_OK)
+async def update_product(update_product: UpdateProduct):
+    # TODO: update database
+    return {}
+
+
+# Update
+@router.delete("/", status_code=status.HTTP_204_NO_CONTENT):
+async def delete_product(id: UUID):
+    # TODO: update database
+    # get product by id from db
+    # if not item_id 
+    return {}
