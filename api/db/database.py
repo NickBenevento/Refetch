6a87cbf16a368ed91c2620102a19a96e8f60e450
Nick Benevento <nbenevento99@gmail.com>
2025-04-28 01:47:30 -0400

    Update connection info with host

--- api/db/database.py
+++ api/db/database.py
@@ -9,20 +9,29 @@ from sqlmodel import Session, SQLModel, create_engine
 load_dotenv()
 
 
-DATABASE_HOST = os.getenv("DB_HOST", "")
+DB_HOST = os.getenv("DB_HOST", "")
 DB_NAME = os.getenv("DB_NAME", "")
 USERNAME = os.getenv("DB_USERNAME", "")
 PASSWORD = os.getenv("DB_PASSWORD", "")
 PORT = os.getenv("DB_PORT", "")
 
 DATABASE_URL = URL.create(
-    "postgresql", username=USERNAME, password=PASSWORD, port=PORT, database=DB_NAME
+    drivername="postgresql",
+    host=DB_HOST,
+    username=USERNAME,
+    password=PASSWORD,
+    port=PORT,
+    database=DB_NAME,
 )
+print("db url: ", DATABASE_URL.render_as_string(hide_password=False))
 
 # allow FastAPI to use the same database in different threads
 # Necessary b/c one single request could use multiple threads
 connect_args = {"check_same_thread": False}
 engine = create_engine(DATABASE_URL)
+# engine = create_engine(
+#     "postgresql://postgres:tango9ij(IJ@refetch.cr04sckw0441.us-east-1.rds.amazonaws.com:5432/postgres"
+# )
 
 
 def create_db_and_tables():
