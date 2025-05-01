b12408d25fe6eaaa771568743a4e162aa33cf036
Nick <nick.benevento@blackcape.io>
2025-04-30 23:25:32 -0400

    Cleanup

--- api/models/user.py
+++ api/models/user.py
@@ -2,11 +2,7 @@ import uuid
 from typing import Annotated
 
 from pydantic import AfterValidator, AliasChoices
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
 from sqlmodel import Field, SQLModel
-
-from api.models.db.base import Base
 
 
 def validate_email(value: str):
@@ -34,17 +30,3 @@ class User(SQLModel, table=True):
     email: str = Annotated[
         str, AfterValidator(validate_email), Field(index=True), "Email"
     ]
-
-
-class User2(Base):
-    __tablename__ = "user"
-
-    id: Mapped[UUID] = mapped_column(primary_key=True)
-    first_name: Mapped[str] = mapped_column(String(20))
-    last_name: Mapped[str] = mapped_column(String(20))
-    email: Mapped[str] = mapped_column(String(50))
-
-    # TODO: add relationship to "subscription" table
-
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.first_name!r} {self.last_name!r}, email={self.email!r})"
