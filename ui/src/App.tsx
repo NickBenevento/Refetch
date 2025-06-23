378715b731cf6c72dd9b11081fac7ff7d3daad52
Nick <nick.benevento@blackcape.io>
2025-06-23 19:18:18 -0400

    Add product list to homepage

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './components/header'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./components/header";
+import "./App.css";
+import ProductList from "./components/product/ProductList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <div className="w-screen min-h-screen bg-cyan-700 items-center justify-center flex flex-col">
       <Header />
+      <ProductList />
       <div className="flex">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
